1b7cffb0433d5f8cc5aa3d2f091889f646ee6958
mshinkle <mshinkle@nevada.unr.edu>
2024-08-09 12:04:26 -0700

    fix: bad setup

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -9,17 +9,18 @@ requirement_path = lib_folder + '/requirements.txt'
 if os.path.isfile(requirement_path):
     with open(requirement_path) as f:
         install_requires = f.read().splitlines()
-setuptools.setup(
+setup(
     name="interdim",
     install_requires=install_requires,
     version="0.0.1",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     author="Matthew Shinkle",
     author_email="matthewshinkle@gmail.com",
     description="InterDim is a streamlined package for interactive exploration of latent data dimensions.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/MShinkle/interdim.git",
-    packages=['interdim'],
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
