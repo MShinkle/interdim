a195816783919379f1ca2183539c4efe2fa75ada
Matthew Shinkle <matthewshinkle@gmail.com>
2024-08-14 15:16:37 -0700

    docs: update versioning

--- src/interdim/__init__.py
+++ src/interdim/__init__.py
@@ -1,4 +1,10 @@
-# __init__.py
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "unknown"
 
 from .pipeline import InterDimAnalysis, analyze_and_visualize
 from . import reduce
